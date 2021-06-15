a9a90198187197bf6614be4db95bc1f86f25762b
MohammadHossein <themhsalimi@gmail.com>
2021-06-15 14:30:02 +0430

    build: make module executable as script

--- Utils/txt2csv/txt2csv.py
+++ Utils/txt2csv/txt2csv.py
@@ -1,5 +1,27 @@
+"""
+A simple script that converts text file to csv file
+
+pass the text file address as a system argument and
+
+the rest will happen!
+"""
 import pandas as pd
+import sys
 
-dataframe = pd.read_csv("grb_table.txt", sep="\t")
 
-dataframe.to_csv("grb_table.csv", index=None)
+def txt2csv(file):
+    """convert text file to csv file
+
+    Args:
+        file (text file): a text file which you want to convert
+
+    Returns:
+        csv file: converted csv file
+    """
+    dataframe = pd.read_csv(file, sep="\t")
+
+    return dataframe.to_csv(str(file).replace(".txt", ".csv"), index=None)
+
+
+if __name__ == "__main__":
+    txt2csv(sys.argv[1])
