9eb6d6a6eab26425f90765bfb18fb406096f43d1
MohammadHossein <59168670+Perun21@users.noreply.github.com>
2021-06-28 17:26:14 +0430

    build: accept argv for deltaZ

--- Utils/Random Forest For SDSS/delta/deltaZ.py
+++ Utils/Random Forest For SDSS/delta/deltaZ.py
@@ -2,17 +2,26 @@ from DecisionTree import main_tree
 from RandomForest import main_forest, plot_forest
 import numpy as np
 from matplotlib import pyplot as plt
+import sys
 
-data = np.load("sdss_galaxy_colors.npy")
 
-tree = main_tree(data)
-forest = main_forest(data)
-plt.scatter(tree, forest, s=0.4)
-plt.xlim((0, tree.max()))
-plt.ylim((0, forest.max()))
-plt.xlabel("Tree delta Z")
-plt.ylabel("Forest delta Z")
-plt.savefig("deltaZ", dpi=1200)
-plt.xlim([0, 1])
-plt.ylim([0, 1])
-plt.show()
+def main(data):
+    tree = main_tree(data)
+    forest = main_forest(data)
+    plt.scatter(tree, forest, s=0.4)
+    plt.xlim((0, tree.max()))
+    plt.ylim((0, forest.max()))
+    plt.xlabel("Tree delta Z")
+    plt.ylabel("Forest delta Z")
+    plt.savefig("deltaZ", dpi=1200)
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.show()
+
+
+if __name__ == "__main__":
+    try:
+        data = np.load(sys.argv[1])
+    except:
+        data = np.load("sdss_galaxy_colors.npy")
+    main(data)
