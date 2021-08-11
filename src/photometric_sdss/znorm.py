a11279dccead0e80e67b60c1278a4cd469a69e33
Aidin Momtaz <a.momtaz98@gmail.com>
2021-08-12 01:33:44 +0430

    add:ref

--- src/photometric_sdss/znorm.py
+++ src/photometric_sdss/znorm.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import pylab
 import numpy as np
 from scipy.optimize import curve_fit
+import seaborn as sb
 
+
+from scipy.stats import norm
 z=[]
 z_spec=[]
 znorm=[]
@@ -18,36 +21,45 @@ for i in range(89880):
 deltazaverage=np.mean(znorm)
 print(deltazaverage)
 dfOut = pd.DataFrame(znorm)
-dfOut.to_excel("znorm.xlsx", index=False)
+dfOut.to_excel("znormdata.xlsx", index=False,columns=["znormdata"])
+
 
 
 
 size, scale = 1000, 10
-df=pd.read_excel("znorm.xlsx")
+df=pd.read_excel("znormdata.xlsx")
 
-ax=df['znorm'].plot.hist(grid=False, bins=200, rwidth=0.9,
-                   color='#607c8e')
+# _,ax,_=plt.hist(df['znormdata'], bins=200, rwidth=0.9,
+#                    color='#607c8e')
 
 plt.xlim(-0.5, 0.5)
-list1=[-0.5,0.5]
+list1=np.arange(-1,1,89880)
 width = 1/1.5
 plt.title("\u0394 z distribution")
 plt.xlabel('\u0394 z')
 plt.ylabel('Number')
 # ax.set_yscale('log')
 # plt.grid(axis='y', alpha=0.75)
-plt.savefig("deltaznorm", dpi=1200)
+ax=sb.histplot(df['znormdata'],bins=400,color='cyan')
+# ax.lines[0].set_color('crimson')
+plt.savefig("deltaznorm2", dpi=1200)
 plt.show()
-
-
+'''mu, sigma = norm.fit(df['znormdata'])
+best_fit_line = norm.pdf(ax, mu, sigma)
+plt.plot(ax, best_fit_line)
+plt.savefig("deltaznorm", dpi=1200)
+plt.show()'''
+#gausaisn plot
 # fit bar plot data using curve_fit
-def func(x, a, b, c):
-    # a Gaussian distribution
-    return a * np.exp(-(x-b)**2/(2*c**2))
+# def func(x, a, b, c):
+#     # a Gaussian distribution
+#     return a * np.exp(-(x-b)**2/(2*c**2))
 
-popt, pcov = curve_fit(func, list1, znorm)
+# popt, pcov = curve_fit(func, list1, znorm)
 
-x = np.linspace(-0.5, 0.5, 100)
-y = func(x, *popt)
+# x = np.linspace(-0.5, 0.5, 100)
+# y = func(x, *popt)
 
-plt.plot(x + width/2, y, c='g')
+# plt.plot(x , y, c='g')
+# plt.savefig("deltaznorm", dpi=1200)
+# plt.show()
