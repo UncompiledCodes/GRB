dcc4ca8c5f17d086f93b81aa7f9e4d15a9ade89e
Aidin Momtaz <a.momtaz98@gmail.com>
2021-11-05 18:56:20 +0330

    :deltaz

--- src/photometric_sdss/barplot.py
+++ src/photometric_sdss/barplot.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 
 size, scale = 1000, 10
-df = pd.read_csv("./data/znormdata.csv")
+df = pd.read_csv("./newplot/deltaz_rforest_total.csv")
 
-ax = df["0"].plot.hist(grid=False, bins=200, rwidth=0.9, color="darkred")
+ax = df["redshift"].plot.hist(grid=False, bins=200, rwidth=0.9, color="darkred")
 
 plt.xlim(-0.6, 0.6)
 
@@ -15,4 +15,4 @@ plt.xlabel("\u0394 z")
 plt.ylabel("Number")
 # ax.set_yscale("log")
 # plt.grid(axis='y', alpha=0.75)
-plt.savefig("deltaz_under2", dpi=1200)
+plt.savefig("deltaz_rforest_total", dpi=1200)
